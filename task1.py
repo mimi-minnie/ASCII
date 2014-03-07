cf7b2378315646686ab5e9f58a8466b154cc0cda
mimi-minnie <adobbs38@gmail.com>
2014-03-07 14:27:45 +0000

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,19 @@
+print ('a' in 'boat')
+#expected outcome is
+print ('c' in 'boat')
+#expected outcome is
+print ('2' + '2')
+#expected outcome is
+print (4*'4')
+#expected outcome is
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print(len(alphabet))
+#expected outcome is
+print(alphabet[1])
+#expected outcome is
+print (alphabet[4:7])
+#expected outcome is
+print (alphabet[0:6:2])
+#expected outcome is
+print (alphabet[::-7])
+#expected outcome is
